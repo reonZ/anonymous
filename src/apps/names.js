7f07d75707f3455c1c0dbaa83e0c0596a162c078
Idle <651462+reonZ@users.noreply.github.com>
2024-07-09 12:25:27 +0200

    fixes #26

--- src/apps/names.js
+++ src/apps/names.js
@@ -1,37 +1,37 @@
-import { formatUnknown, getSavedNames } from '../api'
-import { localize, setSetting, subLocalize, templatePath } from '../module'
+import { formatUnknown, getSavedNames } from "../api";
+import { localize, setSetting, subLocalize, templatePath } from "../module";
 
 export class AnonymousNamesMenu extends FormApplication {
     static get defaultOptions() {
-        return mergeObject(super.defaultOptions, {
-            id: 'anonymous-names-menu',
-            title: localize('templates.names.title'),
-            template: templatePath('names.html'),
+        return foundry.utils.mergeObject(super.defaultOptions, {
+            id: "anonymous-names-menu",
+            title: localize("templates.names.title"),
+            template: templatePath("names.html"),
             width: 400,
-        })
+        });
     }
 
     getData(options) {
-        const unknown = localize('unknown')
-        const saved = getSavedNames()
-        const types = game.system.documentTypes.Actor.map(x => ({
+        const unknown = localize("unknown");
+        const saved = getSavedNames();
+        const types = Object.keys(game.system.documentTypes.Actor).map((x) => ({
             type: x,
-            value: (saved[x] ?? '').trim(),
+            value: (saved[x] ?? "").trim(),
             placeholder: formatUnknown(unknown, x),
-        }))
+        }));
         return {
             ...super.getData(options),
             types,
-            i18n: subLocalize('templates.names'),
-        }
+            i18n: subLocalize("templates.names"),
+        };
     }
 
     activateListeners(html) {
-        super.activateListeners(html)
-        html.find('[data-action=cancel]').on('click', () => this.close())
+        super.activateListeners(html);
+        html.find("[data-action=cancel]").on("click", () => this.close());
     }
 
     async _updateObject(event, formData) {
-        setSetting('names', formData)
+        setSetting("names", formData);
     }
 }
